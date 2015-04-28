f46b12ef091b333fae0e9c928a9cefd47496008a
Morten <sfrandsen8@gmail.com>
2015-04-28 14:24:44 +0200

    Fisse Fisse Fez Fez

---
+++ fag/Noget.cl
@@ -0,0 +1,20 @@
+__kernel void Noget(__global int *a,
+					__global int *b,
+					__global int *result
+					){
+	
+	int local_x = get_local_id(0);
+	int global_x = get_global_id( 0);
+
+	//printf("hgh\n");
+
+	result[global_x] = a[global_x] + b[global_x];
+}
+
+__kernel void Noget2(__global int *ma,
+					 uint scalar){
+
+	int global_x = get_global_id( 0);
+
+	ma[ global_x]     	*= scalar;
+}
---
+++ fag/OpenCLTest.c
@@ -0,0 +1,78 @@
+#include "simpleCL.h"
+
+typedef struct{
+	int cols;
+	int rows;
+	int dataSize;
+	void* dataStart;
+} matrix;
+
+void printMatrix(matrix ma){
+
+	for(int i = 0; i < ma.cols*ma.rows; i++){
+	printf("%d %c", ((int*)ma.dataStart)[i], i % ma.cols == ma.cols - 1 ? '\n' : ' ');
+}
+}
+
+int main(){
+	
+sclHard* allHardware;
+sclHard hardware;
+sclSoft software;
+
+matrix ma;
+ma.cols = 4;
+ma.rows = 4;
+ma.dataSize = sizeof(int);
+ma.dataStart = calloc(ma.cols*ma.rows, ma.dataSize);
+
+for (int i = 0; i < ma.rows; ++i)
+{
+	for (int j = 0; j < ma.cols; ++j)
+	{
+		((int*)ma.dataStart)[i*ma.cols+j] = j;
+	}
+}
+
+printMatrix(ma);
+/*
+int a[] = {1,2,3,4,5,6,7,8,9};
+int b[] = {9,8,7,6,5,4,3,2,1};
+int result[9];
+
+size_t global_size[2];
+size_t local_size[2];
+size_t dataLength = 9;
+size_t dataSize = sizeof(int)*dataLength;
+
+global_size[0] = dataLength;
+local_size[0] = 1;
+global_size[1] = 1;
+local_size[1] = 1;*/
+
+size_t global_size[2];
+size_t local_size[2];
+size_t dataLength = ma.cols*ma.rows;
+size_t dataSize = ma.dataSize*dataLength;
+
+global_size[0] = dataLength;
+local_size[0] = 4;
+global_size[1] = 1;
+local_size[1] = 1;
+int scalar = 2;
+
+int found = 0;
+allHardware = sclGetAllHardware(&found);
+hardware = sclGetFastestDevice(allHardware, found);
+software = sclGetCLSoftware("Noget.cl", "Noget2", hardware);
+
+/*sclManageArgsLaunchKernel(hardware, software, global_size, local_size, "%R %R %R", 
+	dataSize, a, dataSize, b, dataSize, result);
+*/
+sclManageArgsLaunchKernel(hardware, software, global_size, local_size, "%R %a", 
+	dataSize, ma.dataStart, sizeof(int) ,&scalar);
+printf("\n");
+
+printMatrix(ma);
+
+}
